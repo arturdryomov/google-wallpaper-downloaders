0382d58f979f21c1921d9b0af83a1cdb96f44a6b
Artur Dryomov <artur.dryomov@gmail.com>
2015-09-07 15:47:14 +0300

    Change download source.
    Use direct JPG downloads instead of Data URI and Base64 parsing.

--- maps/download-wallpapers.py
+++ maps/download-wallpapers.py
@@ -17,14 +17,13 @@ limitations under the License.
 '''
 
 
-import base64
 import json
 import os
 import urllib2
 import urlparse
 
 
-REMOTE_URL = "https://www.gstatic.com/prettyearth/"
+REMOTE_URL = "https://earthview.withgoogle.com/download/"
 REMOTE_IDS_PATH = "ids.json"
 
 LOCAL_PATH = "wallpapers"
@@ -33,17 +32,15 @@ LOCAL_PATH = "wallpapers"
 def download_wallpapers():
     create_wallpapers_path()
 
-    print(":: Downloading Google Maps wallpapers.") 
+    print(":: Downloading Google Maps wallpapers.")
 
     for wallpaper_id in get_wallpaper_ids():
         print(wallpaper_id)
 
-        wallpaper_info = get_wallpaper_info(wallpaper_id)
-
+        wallpaper_url = get_wallpaper_url(wallpaper_id)
         wallpaper_path = get_wallpaper_path(wallpaper_id)
-        wallpaper_bytes = get_wallpaper_bytes(wallpaper_info)
 
-        download_wallpaper(wallpaper_path, wallpaper_bytes)
+        download_wallpaper(wallpaper_url, wallpaper_path)
 
 
 def create_wallpapers_path():
@@ -68,32 +65,24 @@ def get_wallpaper_ids_path():
     return os.path.join(os.path.dirname(__file__), REMOTE_IDS_PATH)
 
 
-def get_wallpaper_info(wallpaper_id):
-    wallpaper_info_url = get_wallpaper_info_url(wallpaper_id)
-
-    return json.load(urllib2.urlopen(wallpaper_info_url))["dataUri"]
-
-
-def get_wallpaper_info_url(wallpaper_id):
-    return urlparse.urljoin(REMOTE_URL, "{id}.json".format(id=wallpaper_id))
+def get_wallpaper_url(wallpaper_id):
+    return urlparse.urljoin(REMOTE_URL, get_wallpaper_name(wallpaper_id))
 
 
 def get_wallpaper_path(wallpaper_id):
     wallpapers_path = get_wallpapers_path()
-    wallpaper_filename = "{id}.jpg".format(id=wallpaper_id)
+    wallpaper_name = get_wallpaper_name(wallpaper_id)
 
-    return os.path.join(wallpapers_path, wallpaper_filename)
-
-
-def get_wallpaper_bytes(wallpaper_info):
-    bytes_start_position = wallpaper_info.index(",") + 1
-
-    return base64.b64decode(wallpaper_info[bytes_start_position:])
+    return os.path.join(wallpapers_path, wallpaper_name)
 
 
-def download_wallpaper(wallpaper_path, wallpaper_bytes):
+def get_wallpaper_name(wallpaper_id):
+    return "{id}.jpg".format(id=wallpaper_id)
+
+
+def download_wallpaper(wallpaper_url, wallpaper_path):
     with open(wallpaper_path, "wb") as wallpaper_file:
-        wallpaper_file.write(wallpaper_bytes)
+        wallpaper_file.write(urllib2.urlopen(wallpaper_url).read())
 
 
 if __name__ == "__main__":
